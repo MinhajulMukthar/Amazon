4685b61f9bb84421ae5731780536ddefaaba7df8
MinhajulMukthar <minhajminhaj291@gmail.com>
2025-07-31 09:54:11 +0530

    generate  oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,87 @@
+function Cart(cartkey) {
+  const cart = {
+    cartItem: undefined,
+    loadFromStorage() {
+      this.cartItem = JSON.parse(localStorage.getItem(cartkey));
+      if (!this.cartItem) {
+        this.cartItem = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '2'
+        }];
+      }
+    },
+    // Short hand method for writing method in object
+    saveToStorage() {
+      localStorage.setItem('cartItem', JSON.stringify(cart));
+    },
+
+    addToCart(productId) {
+      let matchingItem;
+
+      this.cartItem.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        this.cartItem.push({
+          productId: productId,
+          quantity: 1,
+          deliveryOptionId: '1'
+        });
+      }
+
+      this.saveToStorage();
+    },
+
+    removeFromCart(productId) {
+      const newCart = [];
+
+      this.cartItem.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newCart.push(cartItem);
+        }
+      });
+
+      this.cartItem = newCart;
+
+      this.saveToStorage();
+    },
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+
+      this.cartItem.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      matchingItem.deliveryOptionId = deliveryOptionId;
+
+      this.saveToStorage();
+    }
+  };
+  return cart;
+}
+
+
+const cart = Cart('cart-oop');
+const busniessCart = Cart('cart-business');
+
+
+cart.loadFromStorage();
+busniessCart.loadFromStorage();
+
+cart.addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+
+console.log(cart);
+console.log(busniessCart);
